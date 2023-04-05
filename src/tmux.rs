b211e1880a3d39c189faf6b7836a3d6d9c5025e3
Oscar C <oacs1221@gmail.com>
2023-04-04 23:38:12 -0400

    fix: resolve case if it is running on tmux or not

--- src/tmux.rs
+++ src/tmux.rs
@@ -1,25 +1,32 @@
-use std::path::PathBuf;
+use std::{env, fs::File, io::Write, path::PathBuf};
 
 use tmux_interface::{
-    Error, NewSession, Sessions, SwitchClient, TargetSession, TmuxOutput, SESSION_ALL,
+    AttachSession, Error, NewSession, Sessions, StartServer, SwitchClient, TargetSession,
+    TmuxOutput, SESSION_ALL,
 };
 
 pub fn attach_or_create_tmux_session(dir: PathBuf) -> Result<TmuxOutput, Error> {
+    let mut output = File::create("log")?;
+    StartServer::new().output()?;
     let session_name = dir.file_name().unwrap().to_str().unwrap().to_string();
+    writeln!(output, "session_name: {}", session_name)?;
     let target_session = TargetSession::Raw(&session_name).to_string();
-    let sessions = Sessions::get(SESSION_ALL).unwrap();
+    let sessions = Sessions::get(SESSION_ALL)?;
     let session = sessions
         .into_iter()
         .find(|s| s.name == Some(target_session.to_string()));
-    if let Some(_) = session {
-        return SwitchClient::new().target_session(target_session).output();
-    } else {
+    if session.is_none() {
         NewSession::new()
             .detached()
             .session_name(session_name.clone())
             .start_directory(dir.to_str().unwrap().to_string())
+            .shell_command("nvim .")
             .output()
             .unwrap();
-        SwitchClient::new().target_session(target_session).output()
     }
+
+    if env::var("TMUX").is_err() {
+        AttachSession::new().target_session(session_name).output()?;
+    }
+    SwitchClient::new().target_session(target_session).output()
 }
