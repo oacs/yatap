1bebdb49e81920d8e4a95bc0ba0e8dbbcc252cd8
Oscar Alfredo Castillejo Salazar <oacs1221@gmail.com>
2023-04-06 10:40:49 -0400

    feat: Improve tmux attach session and overall integration (#2)
    * fix: resolve case if it is running on tmux or not
    
    * patch: improve error handling tmux
    
    * feat: path arg to directly attach
    
    * feat: scroll with ctrl-n/p
    
    * patch: improve ui handle events
    
    * refactor: improve code
    
    * refactor: move logic to state
    
    * chore: add some comments

--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,10 @@
 use std::fs::read_dir;
 
+use crossterm::event::KeyCode;
+
 use crate::config::Config;
 
 pub enum InputMode {
-    Normal,
     Insert,
 }
 
@@ -13,24 +14,58 @@ pub struct App {
     pub input: String,
     /// Current input mode
     pub input_mode: InputMode,
+    /// Current selection on the list
     pub selection_index: usize,
 
+    /// First loaded paths value
     base_paths: Vec<String>,
     pub paths: Vec<String>,
 
+    /// toggle to close the app
     pub should_close: bool,
 }
 
 impl App {
-    pub fn search_dirs(&self) -> Vec<String> {
-        if self.input.is_empty() {
-            return self.base_paths.clone();
+    /// Adds a character to the input buffer or removes the previous character if the backspace key is pressed.
+    pub fn add_input_char(&mut self, c: KeyCode) {
+        match c {
+            KeyCode::Backspace => {
+                self.input.pop();
+            }
+            KeyCode::Char(c) => {
+                self.input.push(c);
+            }
+            _ => {}
         }
-        self.base_paths
-            .iter()
-            .filter(|dir| dir.contains(&self.input))
-            .cloned()
-            .collect()
+        self.paths = self.search_dirs();
+    }
+
+    /// Increments the selection index to move to the next item in the list of paths, if there is one.
+    pub fn select_next_item(&mut self) {
+        if self.paths.len() > self.selection_index + 1 {
+            self.selection_index += 1;
+        }
+    }
+
+    /// Decrements the current selection index by one to select the previous item in the list.
+    pub fn select_prev_item(&mut self) {
+        if self.selection_index > 0 {
+            self.selection_index -= 1;
+        }
+    }
+
+    /// This function searches through the directories and returns a vector of the directories that match the search input. If the search input is empty, it returns all the base paths.
+    pub fn search_dirs(&mut self) -> Vec<String> {
+        self.selection_index = 0;
+        if self.input.is_empty() {
+            self.base_paths.clone()
+        } else {
+            self.base_paths
+                .iter()
+                .filter(|dir| dir.contains(&self.input))
+                .cloned()
+                .collect()
+        }
     }
 
     pub(crate) fn from(config: Config) -> App {
@@ -40,10 +75,7 @@ impl App {
             .filter_map(|path| read_dir(path).ok())
             .flat_map(|dir| dir.filter_map(Result::ok))
             .filter(|entry| entry.path().is_dir())
-            .map(|entry| {
-                let path = entry.path();
-                path.to_str().unwrap().to_string()
-            })
+            .map(|entry| entry.path().to_string_lossy().into_owned())
             .collect();
         App {
             selection_index: 0,
@@ -61,7 +93,7 @@ impl Default for App {
         App {
             selection_index: 0,
             input: String::new(),
-            input_mode: InputMode::Normal,
+            input_mode: InputMode::Insert,
             paths: Vec::new(),
             base_paths: Vec::new(),
             should_close: false,
--- src/tmux.rs
+++ src/tmux.rs
@@ -1,25 +1,75 @@
-use std::path::PathBuf;
+use anyhow::anyhow;
+use anyhow::Result;
+use std::{env, path::PathBuf};
 
 use tmux_interface::{
-    Error, NewSession, Sessions, SwitchClient, TargetSession, TmuxOutput, SESSION_ALL,
+    AttachSession, NewSession, Sessions, StartServer, SwitchClient, TargetSession, TmuxOutput,
+    SESSION_ALL,
 };
 
-pub fn attach_or_create_tmux_session(dir: PathBuf) -> Result<TmuxOutput, Error> {
-    let session_name = dir.file_name().unwrap().to_str().unwrap().to_string();
+/**
+ * This function attaches to an existing tmux session if it exists with the same name as the name provided directory.
+ * If a session does not exist, a new one is created with that name and launched with "nvim ."
+ * If we are not on a tmux session, the function attaches to the newly created or existing session.
+ *
+ * @param dir A PathBuf representing the directory to attach or create the tmux session in
+ *
+ * @returns A Result containing TmuxOutput if the function succeeds, otherwise an error is returned
+ *
+ * Errors:
+ *  - Failed to create tmux server: if creating a new tmux server fails
+ *  - Failed to get file name from directory: if the file name cannot be extracted from the provided directory
+ *  - Failed to convert file name to string: if the file name cannot be converted to a string
+ *  - Failed to create new tmux session: if creating a new tmux session fails
+ *  - Failed to switch to client: if switching to the client fails
+ */
+pub fn attach_or_create_tmux_session(dir: PathBuf) -> Result<TmuxOutput> {
+    // Will start server if it is off
+    StartServer::new()
+        .output()
+        .map_err(|e| anyhow!("Failed to create tmux server: {}", e))?;
+
+    // Get the name of the session to be created or attached to
+    let session_name = dir
+        .file_name()
+        .ok_or_else(|| anyhow!("Failed to get file name from directory"))?
+        .to_str()
+        .ok_or_else(|| anyhow!("Failed to convert file name to string"))?
+        .to_string();
+
+    // Check if a session with the same name already exists
     let target_session = TargetSession::Raw(&session_name).to_string();
-    let sessions = Sessions::get(SESSION_ALL).unwrap();
-    let session = sessions
-        .into_iter()
-        .find(|s| s.name == Some(target_session.to_string()));
-    if let Some(_) = session {
-        return SwitchClient::new().target_session(target_session).output();
-    } else {
+    let session = Sessions::get(SESSION_ALL)
+        .map(|s| {
+            s.into_iter()
+                .map(|s| s.name)
+                .filter_map(|mut s| s.take())
+                .find(|s| *s == target_session)
+                .take()
+        })
+        .unwrap_or(None);
+
+    // If no session exists with the same name, create a new one
+    if session.is_none() {
         NewSession::new()
             .detached()
             .session_name(session_name.clone())
             .start_directory(dir.to_str().unwrap().to_string())
+            .shell_command("nvim .")
             .output()
-            .unwrap();
-        SwitchClient::new().target_session(target_session).output()
+            .map_err(|e| anyhow!("Failed to create new tmux session: {}", e))?;
     }
+
+    // If we are not on a tmux session, the function attaches to the newly created or existing session.
+    if env::var("TMUX").is_err() {
+        AttachSession::new()
+            .target_session(&session_name)
+            .output()?;
+    }
+
+    // Switch to the client for the target session
+    SwitchClient::new()
+        .target_session(&target_session)
+        .output()
+        .map_err(|e| anyhow!("Failed to switch to client: {}", e))
 }
